c7cb10c793d3b5b803ad69056a8115c91e81b05b
Dona Biswas <79658668+DonaBiswas45@users.noreply.github.com>
2026-06-02 13:36:05 +0000

    c++

---
+++ http_parser/build/CMakeFiles/http_parser_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/http_parser_lib.dir/src/http_parser.cpp.o"
+  "CMakeFiles/http_parser_lib.dir/src/http_parser.cpp.o.d"
+  "libhttp_parser_lib.a"
+  "libhttp_parser_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/http_parser_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
